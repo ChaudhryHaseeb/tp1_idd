2fce301d0c1eedd12b7cab53765127abf88f1444
Haseeb Chaudhry <haseeb.chaudhry@hotmail.fr>
2019-12-19 12:35:43 +0100

    Add

--- src/main/java/Mediateur.java
+++ src/main/java/Mediateur.java
@@ -9,6 +9,7 @@ public class Mediateur
 
     public void req1()
     {
+        System.out.println("------Requete 1-------");
         list = mysqlExtract.req1RockMysql();
         list.addAll(excelExtract.req1RockExcel());
         for(int i=0; i<list.size();i++)
@@ -42,13 +43,38 @@ public class Mediateur
 
     public void req2()
     {
-
+        System.out.println("------Requete 2-------");
+        list = mysqlExtract.req2Mysql();
+        //list.addAll(excelExtract.req2Excel());
+        for(int i=0; i<list.size();i++)
+        {
+            if(i%2==0)
+            {
+                System.out.print(list.get(i)+",");
+            }
+            else
+            {
+                System.out.println(list.get(i));
+            }
+        }
     }
 
     public void req3()
     {
-
-
+        System.out.println("------Requete 3-------");
+        list = mysqlExtract.req3Mysql();
+        //list.addAll(excelExtract.req3Excel());
+        for(int i=0; i<list.size();i++)
+        {
+            if(i%2==0)
+            {
+                System.out.print(list.get(i)+",");
+            }
+            else
+            {
+                System.out.println(list.get(i));
+            }
+        }
     }
 
 }
